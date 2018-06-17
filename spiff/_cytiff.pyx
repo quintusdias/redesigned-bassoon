75cdab0a12d376c8fba9862d94035f6fdfe68906
John Evans <john.g.evans.ne@gmail.com>
2018-06-16 22:03:23 -0400

    Convert to cpp

--- spiff/_cytiff.pyx
+++ spiff/_cytiff.pyx
@@ -1,21 +1,29 @@
+#cython: language=c++
+
+from libcpp.string cimport string
 from libc.stdio cimport FILE, fopen, fread, printf, fscanf, tmpfile, rewind, ftell
 
 cimport tiff
 
 cpdef print_directory(unsigned long tiffp):
     cdef FILE *fp
+    cdef char *buffer
     cdef long numbytes
     cdef bytes py_string
+    cdef string s
 
-    cdef char str[1024]
+    # cdef char str[1024]
 
     fp = tmpfile()
     tiff.TIFFPrintDirectory(<tiff.TIFF *>tiffp, fp, 0)
 
     numbytes = ftell(fp)
+    s.reserve(numbytes)
 
     # Rewind and reread the contents.
     rewind(fp)
-    fread(str, 1, numbytes, fp)
-    py_string = str
+
+    fread(&s[0], 1, numbytes, fp)
+    cdef char *c_string = <char *>s.c_str()
+    py_string = c_string
     return py_string
